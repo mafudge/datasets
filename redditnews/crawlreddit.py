d62ddcbfca0bad009cf273f5217d3db93642f9a9
Michael Fudge <Michael Fudge>
2016-03-24 23:15:57 -0400

    fixed crawl reddit

--- redditnews/crawlreddit.py
+++ redditnews/crawlreddit.py
@@ -44,7 +44,7 @@ def subredditInfo(client, limit=25, after='', sr='news',
     if it is: Allowed passing params/queries such as t=hour, week, month, year or all"""
  
     #query to send
-    parameters = {'limit': limit, 'after' : after, }
+    parameters = {'limit': limit, 'after' : after,  't' : 'all'}
     #parameters= defaults.copy()
     parameters.update(kwargs)
  
@@ -52,6 +52,8 @@ def subredditInfo(client, limit=25, after='', sr='news',
     r = client.get(url,params=parameters)
     print ('sent URL is', r.url)
     j = json.loads(r.text)
+    ratelimitremaining = int(r.headers['x-ratelimit-remaining'])
+    ratelimitreset = int(r.headers['x-ratelimit-reset'])
  
     #return raw json
     if return_json:
@@ -64,18 +66,25 @@ def subredditInfo(client, limit=25, after='', sr='news',
             #print story['data']['title']
             stories.append(story)
  
-        return stories
+        return ratelimitremaining,ratelimitreset,stories
  
+########## main ##########
 client = login('mafudge', 'xrnT9sO04RaCu6MX7bxn')
- 
+last = '' 
+for i in range(1,8):
+    filename = 'top.json' if last == '' else last + '.json'
+    with open(filename,"w", encoding='utf-8') as appendfile:
+        rlimit,reset, stories = subredditInfo(client, sr='news', limit=100, after=last)
+        titles = [(s['data']['name'], s['data']['title']) for s in stories ]
+        print(rlimit, reset, titles)
+        appendfile.write(json.dumps(stories))
+        last = stories[-1]['data']['name']
 
-s1 = subredditInfo(client, sr='news', limit=2)
-last = s1[-1]['data']['name']
-s2 = subredditInfo(client, sr='news', limit=2, after=last)
+#r2,s2 = subredditInfo(client, sr='news', limit=1, after=last)
 
-#t1 = [(s['data']['name'], s['name']['title']) for s in s1 ]
-#t2 = [(s['data']['name'], s['name']['title']) for s in s2 ]
-
+#t2 = [(s['data']['name'], s['data']['title']) for s in s2 ]
+#pp2(t1)
+#pp2(t2)
 
 
 
